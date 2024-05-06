a468d3863a238e5b88dbec044c865bdd83dfde04
Mohammad-hosein Malekian <mh.malekian78@gmail.com>
2024-05-06 07:38:50 +0430

    registering and customizing the Custom User in admin

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    # setting filds of what to showing in admin-dashboard
+    model = User
+    list_display = ("email", "is_superuser", "is_active", "created_date")
+    list_filter = ("email", "is_superuser", "is_active", "created_date")
+    search_fields = ("email",)
+    ordering = ("created_date",)
+    fieldsets = (
+        ('Authentication', {
+            'fields': (
+                "email", "password"
+            ),
+        }),
+        ('Permissions', {
+            'fields': (
+                "is_staff", "is_active","is_superuser"
+            ),
+        }),
+    )
+    # setting fields for adding and save new User by admin-dashboard
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email", "password1", "password2", "is_staff",
+                "is_active", "is_superuser"
+            )}
+        ),
+    )
+
+admin.site.register(User, CustomUserAdmin)
