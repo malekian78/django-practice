9b4b16d99a63ecde7607eca0ec3197255510d73f
Mohammad-hosein Malekian <mh.malekian78@gmail.com>
2024-05-07 22:18:41 +0430

    ✅ Task Checked/Complete added

--- core/todo/views.py
+++ core/todo/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.list import ListView
 from .models import Task
+from django.shortcuts import redirect
+from django.views.generic.list import ListView
+from django.views.generic.edit import (
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+from django.urls import reverse_lazy
+# from .forms import TaskUpdateForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
 
 class TaskList(LoginRequiredMixin, ListView):
     model = Task
@@ -10,3 +20,13 @@ class TaskList(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return self.model.objects.filter(user=self.request.user)
+
+class TaskComplete(LoginRequiredMixin, View):
+    model = Task
+    success_url = reverse_lazy("task_list")
+
+    def get(self, request, *args, **kwargs):
+        object = Task.objects.get(id=kwargs.get("pk"))
+        object.complete = True
+        object.save()
+        return redirect(self.success_url)
