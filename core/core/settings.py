3b4274e049d4a7c2ca8bef31c6342278d3134cfa
Mohammad-hosein Malekian <mh.malekian78@gmail.com>
2024-05-16 22:08:38 +0430

    📦pip install pytest & pip install & pip install pytest-django

--- core/core/settings.py
+++ core/core/settings.py
@@ -22,11 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-7lxdluevo9-k$i-ievs76!=$r$!461w)(3uf07b6_-))y%il7x'
-# SECRET_KEY = config("SECRET_KEY")
+# SECRET_KEY = config("SECRET_KEY", default="test")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-# DEBUG = config("DEBUG", cast=bool)
+# DEBUG = config("DEBUG", cast=bool, Default=True)
 
 ALLOWED_HOSTS = []
 # ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')], default="*")
